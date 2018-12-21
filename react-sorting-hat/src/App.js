967e2e1a839735d550fafa13ec0f8fa339e1d61b
derrick-mei <derrick.mei.168@gmail.com>
2018-12-20 21:04:43 -0600

    implemented fade for questions page

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import QuestionsPage from "./components/questionsPage"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayWelcome: true
+    };
+  }
+
+  handleWelcome = () => {
+    this.setState({ displayWelcome: !this.state.displayWelcome });
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +25,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        {this.state.displayWelcome ? (
+          <button
+            style={{ display: this.state.displayWelcome }}
+            onClick={this.handleWelcome}
+          >
+            Click Me!!!
+          </button>
+        ) : null}
+
+        <button onClick={this.handleWelcome}>Click to go Back</button>
+        {/* {!this.state.displayWelcome?<QuestionsPage/>:null} */}
+        <QuestionsPage className={this.state.displayWelcome?'basket hide': 'basket'}/>
       </div>
     );
   }
---
+++ react-sorting-hat/src/components/questionsPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const QuestionsPage = (props) => {
+    return (
+        <div className={props.className}>testing Questions Page</div>
+    )
+}
+
+export default QuestionsPage;
